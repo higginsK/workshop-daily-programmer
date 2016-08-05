c928bd369aeec8c26e7353b0081f3b6d4c6ae138
K. Higgins <higginsK@users.noreply.github.com>
2016-08-04 18:37:56 -0600

    Add files via upload

---
+++ 02_rampNumbers/02_rampnumbersKH.js
@@ -0,0 +1,20 @@
+//Ramp Numbers
+//A ramp number is a number whose digits from left to right either only rise or stay the same.
+//1234 is a ramp number as is 1124. 1032 is not.
+
+
+//Input
+//A positive integer, n.
+
+//Output
+//true if the input is a ramp number, false if it is not
+
+var num1 = 2222
+
+function ramp(num1) {
+
+  //for (n=0; n<num1.length) {
+  var num1= num1.split('');
+
+}
+console.log(num1)
